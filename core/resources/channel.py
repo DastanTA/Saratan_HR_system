348885d9bf3a0f54104154b1bed6a4eada23f742
Dastan <das.turatbek@gmail.com>
2023-08-22 20:02:42 +0600

    feature/SD-55: endpoint to create a channel is added

--- core/resources/channel.py
+++ core/resources/channel.py
@@ -3,12 +3,23 @@ from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
 from core.db import db
-from core.schemas import PlainChannelSchema
+from core.schemas import PlainChannelSchema, ChannelSchema
 from core.models import ChannelModel
 
 blp = Blueprint("channels", __name__, description="Operations on channels")
 
 
-@blp.route("/project")
+@blp.route("/channel")
 class GetAllAndCreateProject(MethodView):
-    pass
+    @blp.arguments(PlainChannelSchema)
+    @blp.response(201, ChannelSchema)
+    def post(self, channel_data):
+        channel = ChannelModel(**channel_data)
+
+        try:
+            db.session.add(channel)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(400, message=str(e))
+
+        return channel
