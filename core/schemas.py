794257a9b60f645b10e960d7d27ef4c166e7cf52
Dastan <das.turatbek@gmail.com>
2023-08-26 13:03:51 +0600

    feature/SD-69: PlainPositionSchema

--- core/schemas.py
+++ core/schemas.py
@@ -40,6 +40,12 @@ class PlainProjectSchema(Schema):
     project_type_id = fields.Int(required=False)
 
 
+class PlainPositionSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    description = fields.Str(required=False)
+
+
 class ProjectTypeSchema(PlainProjectTypeSchema):
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
